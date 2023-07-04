8bb3ca41598acb51d62068ff8302c88b590f2bcd
MarioSantoro <mariosantoro124@gmail.com>
2023-07-04 16:49:24 +0200

    bonus 2 complete

--- function.php
+++ function.php
@@ -1,6 +1,27 @@
 <?php
-function createPassword($letters, $lenghtPassword)
+function createPassword($lettere, $numeri, $simboli, $lenghtPassword)
 {
-    $password = substr(str_shuffle($letters), 0, $lenghtPassword);
+    $_characters = 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm';
+    $number = '0123456789';
+    $symbol = '!&/*+-.:';
+    $all = 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm0123456789!&/*+-.:';
+    $numberAndCharacters = 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm0123456789';
+    $symbolAndCharacters = 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm!&/*+-.:';
+    $symbolAndNumber = '!&/*+-.:0123456789';
+    if ($lettere === 'on' && !empty($lettere) && empty($numeri) && empty($simboli)) {
+        $password = substr(str_shuffle($_characters), 0, $lenghtPassword);
+    } elseif ($numeri === 'on' && !empty($numeri) && empty($lettere) && empty($simboli)) {
+        $password = substr(str_shuffle($number), 0, $lenghtPassword);
+    } elseif ($simboli === 'on' && !empty($simboli) && empty($numeri) && empty($lettere)) {
+        $password = substr(str_shuffle($symbol), 0, $lenghtPassword);
+    } elseif ($lettere === 'on' && $numeri === 'on'  && $simboli === 'on') {
+        $password = substr(str_shuffle($all), 0, $lenghtPassword);
+    } elseif ($numeri === 'on' && !empty($numeri) && $lettere === 'on' && !empty($lettere) && empty($simboli)) {
+        $password = substr(str_shuffle($numberAndCharacters), 0, $lenghtPassword);
+    } elseif ($lettere === 'on' && !empty($lettere) && $simboli === 'on' && !empty($simboli) && empty($numeri)) {
+        $password = substr(str_shuffle($symbolAndCharacters), 0, $lenghtPassword);
+    } elseif ($numeri === 'on' && !empty($numeri) && $simboli === 'on' && !empty($simboli) && empty($lettere)) {
+        $password = substr(str_shuffle($symbolAndNumber), 0, $lenghtPassword);
+    }
     return $password;
 }
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$_characters = 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm0123456789!&/';
+
 include_once __DIR__ . '/function.php';
 ?>
 
@@ -20,8 +20,8 @@ include_once __DIR__ . '/function.php';
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <?php if (!empty($_GET['lenPassword']) && $_GET['lenPassword'] !== 0) { ?>
-                    <?php $_SESSION['pass'] = createPassword($_characters, $_GET['lenPassword']) ?>
+                <?php if (!empty($_GET['lenPassword'])) { ?>
+                    <?php $_SESSION['pass'] = createPassword($_GET['lettere'], $_GET['numeri'], $_GET['simboli'], $_GET['lenPassword']) ?>
                     <?php header("Location: http://localhost/classe-98/php-strong-password-generator/response.php/");
                     exit;
                     ?>
@@ -34,15 +34,37 @@ include_once __DIR__ . '/function.php';
             <div class="col-12">
                 <div class="container-form">
                     <form action="./index.php" method="get">
-                        <div class="mb-3 d-flex justify-content-between align-items-baseline p-3">
-                            <div class="controls d-flex flex-column">
+                        <div class="mb-3 d-flex justify-content-between p-3">
+                            <div class="controls h-100 d-flex flex-column justify-content-between">
                                 <label for="exampleInputPassword1" class="form-label">Lunghezza password:</label>
                                 <div class="buttons d-flex">
                                     <button type="submit" class="btn btn-primary me-2">Invia</button>
                                     <button type="reset" class="btn btn-secondary">Anulla</button>
                                 </div>
                             </div>
-                            <input type="number" class="form-control w-25" id="exampleInputPassword1" name="lenPassword">
+                            <div>
+                                <input type="number" class="form-control mb-4" id="exampleInputPassword1" name="lenPassword">
+                                <div class="checkboxs d-flex flex-column">
+                                    <div class="check d-flex">
+                                        <input class="form-check-input" type="checkbox" name="lettere" id="flexCheckDefault">
+                                        <label class="form-check-label" for="flexCheckDefault">
+                                            Lettere
+                                        </label>
+                                    </div>
+                                    <div class="check  d-flex">
+                                        <input class="form-check-input" type="checkbox" name="numeri" id="flexCheckDefault">
+                                        <label class="form-check-label" for="flexCheckDefault">
+                                            Numeri
+                                        </label>
+                                    </div>
+                                    <div class="check  d-flex">
+                                        <input class="form-check-input" type="checkbox" name="simboli" id="flexCheckDefault">
+                                        <label class="form-check-label" for="flexCheckDefault">
+                                            Simboli
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </form>
                 </div>
